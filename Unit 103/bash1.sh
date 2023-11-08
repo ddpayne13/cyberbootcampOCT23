dcb763113b7ff6e51aa79cc9bbaf9dd2f488bf80
dante payne <dante.d.payne@gmail.com>
2023-11-07 19:44:43 -0600

    bash1.sh

---
+++ Unit 103/bash1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Script: Ops 201 Class 01 Ops Challenge Solution"
+echo "Author: Dante Payne"
+echo "Date of latest revision: 07 NOV 2023"
+echo "Purpose: Print a string to the terminal"
