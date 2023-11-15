21ca1750877d199abf97c92ad43e19298807bd81
dante payne <dante.d.payne@gmail.com>
2023-11-14 20:09:58 -0600

    updates

--- Unit 103/Lab3.sh
+++ Unit 103/Lab3.sh
@@ -3,10 +3,11 @@
 # This ops challenges is about conditinal stamtment of if else statments and how they work
 # We are going to take a varibale with the number and have the computer tell us if its greater than 5 less than 5 or equals 5
 
-number=7
-if [[ $number > 5 ]]
+echo "Enter number"
+read number
+if [[ $number -gt 5 ]]
     then echo "Number is greater than 5"
-elif [[ $number < 5 ]]
+elif [[ $number -lt 5 ]]
     then echo "Number is less than 5"
 else echo "number is 5"
 fi
