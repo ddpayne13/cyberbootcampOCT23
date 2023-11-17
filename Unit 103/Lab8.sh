659e3a9bcd40be9365a343fd2a49f7f2cedec388
dante payne <dante.d.payne@gmail.com>
2023-11-16 19:15:12 -0600

    Lab 8

--- Unit 103/Lab8.sh
+++ Unit 103/Lab8.sh
@@ -2,3 +2,11 @@
 #Lets create a script that would work like a DDOS attack by using a while loop
 #In this script we want to contiune to ping our personal pc in a infitine while loop
 #Somebody that had a control of a bot net could set up this script on thousands of computer and ping sites till they are overloaded and crash
+
+while :
+do
+    ping 192.168.1.108
+    echo "Keep running"
+    sleep 3
+   done
+   
